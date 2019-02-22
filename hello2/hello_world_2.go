14c26ae3de7b4723a973cf7e1b87f34fe540db4b
qijunyao <qijunyao@xiaohongshu.com>
2019-02-22 17:07:12 +0800

    init all

--- hello2/hello_world_2.go
+++ hello2/hello_world_2.go
@@ -1,7 +1,111 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type T struct {
+	name string
+	value int
+}
+
+func (t T) changeValue() {
+	t.value = 100
+	fmt.Println("defer t content is ", t)
+}
+
+var (
+	first = firstValue()
+	second = "second"
+)
+
+//func init()  {
+//	fmt.Println("first value in init is ", first)
+//	fmt.Println("second value in init is ", second)
+//}
 
 func main() {
-	fmt.Println("hello world 2 condition")
+	//fmt.Println("hello world 2 condition")
+	//t := printHello()
+	//fmt.Println("main function t is ", t)
+	//fmt.Println(t)
+
+	//a := [3]int{1, 2, 3}
+	//fmt.Println(a)
+	//changeCopyArrayContent(a)
+	//fmt.Println(a)
+
+	//slice := []byte{1, 2, 3}
+	//data := []byte{4, 5, 6}
+	//result := Append(slice, data)
+	//fmt.Println(slice, cap(slice), len(slice))
+	//fmt.Println(result, cap(result), len(result))
+	//fmt.Println(data, cap(data), len(data))
+
+	//fmt.Printf("Hello %d\n", 23)
+	//fmt.Fprint(os.Stdout, "Hello ", 23, "\n")
+	//fmt.Println("Hello", 23)
+	//fmt.Println(fmt.Sprint("Hello ", 23))
+
+	//fmt.Println("main constant value")
+
+	//t := new([]int)
+	//fmt.Println(&t)
+
+	//fmt.Println(TwoParamsHandle.handleTow(AddFunction, 1, 2))
+
+	//f := func() {
+	//	fmt.Println("test")
+	//}
+	//f()
+	//fp := &f
+	//(*fp)()
+
+	// 函数仅仅支持指针方式, 没有变量类似的覆盖方式
+	a := adderFn()
+	fmt.Println(a())
+	fmt.Println(a())
+
+	b := adderFn()
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(b())
+	fmt.Println(b())
+}
+
+func adderFn() func() int{
+	a := 0
+	return func() int {
+		a ++
+		return a
+	}
+}
+
+func firstValue() string {
+	fmt.Println("first value function")
+	return "firstValue"
+}
+
+func Append(slice, data []byte) []byte {
+	l := len(slice)
+	if l + len(data) > cap(slice) {  // reallocate
+		// Allocate double what's needed, for future growth.
+		newSlice := make([]byte, (l+len(data))*2)
+		// The copy function is predeclared and works for any slice type.
+		copy(newSlice, slice)
+		slice = newSlice
+	}
+	slice = slice[0:l+len(data)]
+	copy(slice[l:], data)
+	return slice
+}
+
+func changeCopyArrayContent(param [3]int){
+	param[0] = 666
+}
+
+func printHello() *T {
+	t := &T{"yao", 123}
+	defer t.changeValue()
+	return t
 }
--- leetcode/leetcode_run_demo.go
+++ leetcode/leetcode_run_demo.go
@@ -1,10 +1,109 @@
-package leetcode
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	//fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
-	fmt.Println("hello world content")
+	//fmt.Println("hello world content")
+	//fmt.Println(isPalindrome(10))
+	//fmt.Println(longestCommonPrefix([]string{"flower","flow","flight"}))
+	fmt.Println(isValid("(]"))
+}
+
+func isValid(s string) bool {
+	stack := make([]uint8, len(s))
+	pos := 0
+	for i:=0; i<len(s) ; i++ {
+		curr := s[i]
+		if curr == 40 || curr == 123 || curr == 91 {
+			stack[pos] = curr
+			pos += 1
+		}else {
+			if pos == 0 {
+				pos += 1
+				break
+			}
+			toCurr := stack[pos-1]
+
+			switch curr {
+			case 93:
+				if toCurr != 91 {
+					return false
+				}
+			case 125:
+				if toCurr != 123{
+					return false
+				}
+			case 41:
+				if toCurr != 40 {
+					return false
+				}
+			}
+			pos -= 1
+		}
+	}
+	return pos == 0
+}
+
+func longestCommonPrefix(strs []string) string {
+	if strs == nil || len(strs) == 0{
+		return ""
+	}
+
+	s1 := strs[0]
+	commonLength := len(s1)
+
+	for i := 1; i < len(strs) ; i++ {
+		currStr := strs[i]
+		k := 0
+		for k < commonLength && k < len(currStr) {
+			if currStr[k] != s1[k] {
+				break
+			}
+			k++
+		}
+		commonLength = k
+		if commonLength == 0 {
+			break
+		}
+	}
+
+	if commonLength == 0 {
+		return ""
+	}
+	return s1[:commonLength]
+}
+
+func reverse(x int) int {
+
+	var y int64 = int64(x)
+
+	var revert int64 = 0
+
+	for y != 0{
+		revert = revert * 10 + y % 10
+		y = y / 10
+	}
+	if revert <= math.MaxInt32 || revert >= math.MinInt32{
+		return int(revert)
+	}
+	return 0
+}
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	y := x
+	revert := 0
+	for y != 0  {
+		revert = revert * 10 + y % 10
+		y = y / 10
+	}
+	return revert == x
 }
 
 func twoSum(nums []int, target int) []int {
